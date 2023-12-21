b26ca65beac2c1481db849805c4587f6d361ade7
Brendan McStay <mcstaybrendan@gmail.com>
2023-12-21 15:26:01 -0700

    Added options for the user/added alerts

--- script.js
+++ script.js
@@ -9,6 +9,41 @@ var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'
 var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 
+//added options for the user and conditional statments
+function questions() {
+  var isValid = false;
+  do {
+    var length = prompt("Choose password length from 8 to 128 characters");
+    var askuppercase = confirm("Do you want the password to have uppercase letters?");
+    var asklowercase = confirm("Do you want the password to have lowercase letters?");
+    var asknumbers = confirm("Do you want the password to have numbers?");
+    var askspecial = confirm("Do you want the password to have special characters in it?");
+    var responses = {
+      length: length,
+      asklowercase: asklowercase,
+      askuppercase: askuppercase,
+      asknumbers: asknumbers,
+      askspecial: askspecial
+    }
+
+
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
